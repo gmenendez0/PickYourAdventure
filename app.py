07b0d99c53112c1dfee8c54df3ac1f653478915e
Guido Menendez <guidomenendez0@gmail.com>
2024-08-27 22:32:58 -0300

    Finish database integration

--- app.py
+++ app.py
@@ -16,10 +16,6 @@ if __name__ == "__main__":
     app.run(debug=True)
 
 #PENDING:
-#1. Implementar GameService._get_first_adventure()
-    #1.1 Implementar adventureService
-    #1.2 Implementar adventureRepository
-    #1.3 Integrar BDD SQLLite
 #2. Implementar capa controller (con manejo de errores personalizado para poder cambiar de tipo de error)
 #3. Documentar
 #4. Implementar tests unitarios
---
+++ createDatabase.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+def initialize_database(db_path: str):
+    connection = sqlite3.connect(db_path)
+    cursor = connection.cursor()
+
+    # Crear la tabla de aventuras
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS adventures (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        description TEXT NOT NULL
+    );
+    ''')
+
+    # Crear la tabla de opciones de aventuras
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS adventure_options (
+            adventure_id INTEGER NOT NULL,
+            option_id INTEGER NOT NULL,
+            FOREIGN KEY (adventure_id) REFERENCES adventures(id),
+            FOREIGN KEY (option_id) REFERENCES adventures(id),
+            PRIMARY KEY (adventure_id, option_id)
+        );
+    ''')
+
+    connection.commit()
+    connection.close()
+
+initialize_database('db.sqlite3')
